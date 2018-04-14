019bc10cac3617c3cab9de66adc6e515c3ca81b3
Maciej Dobrzanski <reg.github@posterus.com>
2018-04-14 08:32:05 +0200

    Added header

--- game1.ino
+++ game1.ino
@@ -1,6 +1,17 @@
+/*
+ * game1 - My first attempt to create a Gamebuino Classic game
+ * 
+ * Copyright (c) 2018 Maciej Dobrzanski
+ * 
+ * Under GPLv3. See LICENSE file details.
+ * 
+ */
+ 
 #include <SPI.h>
 #include <EEPROM.h>
+
 #include <Gamebuino.h>
+
 Gamebuino gb;
 
 const byte PLAYER[][10] PROGMEM = {
